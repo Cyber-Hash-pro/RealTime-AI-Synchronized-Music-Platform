af819153eebdde01e1c4f79594730e0d8b90e643
Cyber-Rebel <np103177@gmail.com>
2025-12-31 21:40:37 +0530

    feat: add Conversation schema for managing chat messages with expiration one Day

---
+++ TunePal/src/model/chat.model.js
@@ -0,0 +1,18 @@
+const ConversationSchema = new mongoose.Schema({
+  userId: String,
+  messages: [
+    {
+      role: String,
+      message: String
+    }
+  ]
+}, {
+  timestamps: true
+});
+
+ConversationSchema.index(
+  { createdAt: 1 },
+  { expireAfterSeconds: 86400 }
+);
+
+module.exports = mongoose.model("Conversation", ConversationSchema);
