b2cc480408f71c93288d5cbeeb9df8b8f389b52e
Cyber-Rebel <np103177@gmail.com>
2025-12-31 21:33:36 +0530

    refactor: remove unnecessary blank lines in state.js for cleaner code  we want to go keyword based routing to auto routing with ai

--- TunePal/src/agent/graph/state.js
+++ TunePal/src/agent/graph/state.js
@@ -2,12 +2,9 @@ const { Annotation } = require("langgraph");
 
 const State = Annotation.Root({
   messages: Annotation.Messages(),   // Chat conversation memory
-
   mood: Annotation.Scalar(),         // Detected mood (sad, happy, gym, etc)
-
   playlistRequest: Annotation.Any(), // Playlist create request data
   playlist: Annotation.Any(),        // Created / Recommended playlist result
-
   songRequest: Annotation.Any(),     // Song to play / details requested
   song: Annotation.Any(),            // Final song data or playing info
 });
